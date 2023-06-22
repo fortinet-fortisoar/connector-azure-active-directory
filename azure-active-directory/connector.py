d1c5933c7b344d92c78d8bfa847db5763db9b014
usurana <usurana@fortinet.com>
2023-06-22 16:53:25 +0530

    handled macro in connectors.py file

--- azure-active-directory/connector.py
+++ azure-active-directory/connector.py
@@ -6,8 +6,12 @@
 
 from connectors.core.connector import Connector, get_logger, ConnectorError
 from .operations import operations, _check_health
+from django.conf import settings
+from integrations.crudhub import make_request
 
 logger = get_logger('azure-active-directory')
+
+MACRO_LIST = ["User_Enrichment_Playbooks_IRIs"]
 
 
 class AzureAcivedirectory(Connector):
@@ -28,3 +32,27 @@ class AzureAcivedirectory(Connector):
                           "connector_version": self._info_json.get('version')}
         _check_health(config, connector_info)
         logger.info('completed health check no errors')
+
+    def del_micro(self, config):
+        if not settings.LW_AGENT:
+            for macro in MACRO_LIST:
+                try:
+                    resp = make_request(f'/api/wf/api/dynamic-variable/?name={macro}', 'GET')
+                    if resp['hydra:member']:
+                        logger.info("resetting global variable '%s'" % macro)
+                        macro_id = resp['hydra:member'][0]['id']
+                        resp = make_request(f'/api/wf/api/dynamic-variable/{macro_id}/?format=json', 'DELETE')
+                except Exception as e:
+                    logger.error(e)
+
+    def on_deactivate(self, config):
+        self.del_micro(config)
+
+    def on_activate(self, config):
+        self.del_micro(config)
+
+    def on_add_config(self, config, active):
+        self.del_micro(config)
+
+    def on_delete_config(self, config):
+        self.del_micro(config)
